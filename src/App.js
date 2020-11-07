b4d9a8d7a32277873e83c2bdd74720f07ab448d3
Sean Fox <seanpfox2@gmail.com>
2020-11-07 09:52:52 -0700

    Added basic file drop downs.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import { useEffect } from 'react';
 import './App.css';
 
 
-const headings = [
-  { label: "Name", dataProperty: "name" },
-  { label: "Address", dataProperty: "address1" },
-  { label: "City", dataProperty: "city" },
-  { label: "State", dataProperty: "state" },
+const desiredHeadings = [
+  { dataProperty: "name", isFilterable: false },
+  { dataProperty: "city", isFilterable: false },
+  { dataProperty: "state", isFilterable: true },
+  { dataProperty: "telephone", isFilterable: false },
+  { dataProperty: "genre", isFilterable: true },
+  { dataProperty: "attire", isFilterable: true },
 ];
+
 
 function App() {
 
+  const [filter, setFilter] = useState({
+    genre: null,
+    text: '',
+    page: 1,
+  });
 
+  const [headings, setHeadings] = useState(null);
   const [restaurants, setRestaurants] = useState(null);
+  const [filteredRestaurants, setFilteredRestaurants] = useState(null);
 
   useEffect(() => {
 
@@ -23,41 +33,99 @@ function App() {
         Authorization: 'Api-Key q3MNxtfep8Gt',
       },
     })
-    .then((response) => response.json())
-    .then((restaurants) => {
-      console.log(restaurants);
+      .then((response) => response.json())
+      .then((restaurants) => {
+        //console.log(restaurants);
 
-      setRestaurants(restaurants);
-    });
+        const headingKeys = Object.keys(restaurants[0]);
+        //console.log("headingKeys", headingKeys)
+
+        const includedHeadings = desiredHeadings.filter((desiredHeading) => {
+          return !!headingKeys.find(key => desiredHeading.dataProperty === key);
+        })
+
+        //console.log("includedHeadings", includedHeadings)
+
+        setHeadings(includedHeadings);
+
+        setRestaurants(restaurants);
+      });
   }, []);
+
+
+  useEffect(() => {
+    if (restaurants) {
+      let filteredResults = restaurants.filter(restaurant => {
+        let matchesFilter = true;
+        return matchesFilter;
+      });
+      // TODO: get only results for this page with .slice
+
+      ////[filterResults] //which are set in the state
+      setFilteredRestaurants(filteredResults);
+    }
+
+  },
+    [filter.genre, filter.text, filter.page]
+  );
 
 
   return (
     <div className="App">
+
       <table>
         <thead>
-          <tr>
+          <tr className="table-heading">
             {
-              headings.map((heading) => {
-                return <td key={heading.dataProperty}>{heading.label}</td>
+              headings?.map((heading) => {
+                return <td key={heading.dataProperty}>{heading.dataProperty}</td>
               })
             }
           </tr>
         </thead>
 
         <tbody>
-        { 
-          restaurants?.map((restaurant) => {
-            return (
-              <tr key={restaurant.id}>
-                {
-                  headings.map((heading) => {
-                    return <td key={heading.dataProperty}>{ restaurant[heading.dataProperty] }</td>
-                  })
-                }
-              </tr>)
-          })        
-        }
+          <tr className="table-row">
+            {
+              headings?.map((heading) => {
+                return (
+                  <td key={heading.dataProperty}>
+
+                    {
+                      heading.isFilterable ?
+
+                        <select>
+                          <option value={"test"}>test</option>
+
+                          {/* a.map((aO) => {
+                          <option key={aO.dataProperty} value={aO}>{aO}</option>
+                          }) */}
+                        </select>
+
+                        :
+                        null
+
+                    }
+                  </td>
+                )
+              })
+            }
+          </tr>
+        </tbody>
+
+        <tbody>
+          {
+            restaurants?.map((restaurant) => {
+              return (
+                <tr key={restaurant.id} className="table-row">
+                  {
+                    headings.map((heading) => {
+                      return <td key={heading.dataProperty}>{restaurant[heading.dataProperty]}</td>
+                    })
+                  }
+                </tr>)
+            })
+          }
         </tbody>
 
       </table>
@@ -65,5 +133,3 @@ function App() {
   );
 }
 export default App;
-
-// {id: "f223fdd0-4adc-423e-9747-980a66c256ca", name: "Old Hickory Steakhouse", address1: "201 Waterfront St", city: "Oxon Hill", state: "MD", …}
